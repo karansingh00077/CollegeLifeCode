4b3a8e386c38a88f5ef635a56613a56d9ecf4351
karansingh00077 <149601820+karansingh00077@users.noreply.github.com>
2023-11-01 20:47:17 +0530

    Create squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+int a,root;
+printf("enter a number");
+scanf("%d",&a);
+root=sqrt(a);
+printf("the square root of number is =%d",root);
+return 0;
+}
